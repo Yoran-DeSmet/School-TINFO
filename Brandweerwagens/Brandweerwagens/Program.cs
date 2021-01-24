ceaa32cabccd2ce60ce578b8351555c0c695f3eb
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-01-24 16:23:20 +0100

    Brandweerwagens

---
+++ Brandweerwagens/Brandweerwagens/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandweerwagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ KMI/KMI/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 5AIT3 - Yoran - 2021-1-20
+ * 
+ * OPGAVE: 
+ * -------
+ * bouw een lijst met de 10 eerste natuurlijke getallen
+ * Toon deb lijst aan de gebruiker
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] getallenlijst = new byte[10]; // {0,1,2,3,4,5,6,7,8,9 };
+            //Array opvullen
+            for (int i = 0; i < 10; i++)
+            {
+                getallenlijst[i] = i;
+            }
+        }
+    }
+}
