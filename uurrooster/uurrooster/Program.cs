c3f2830ee14a455b3181928fc4eba82636903553
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-01-26 20:54:01 +0100

    uurrooster

---
+++ uurrooster/uurrooster/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uurrooster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] uurrooster = new string[,] { };
+
+            string[] lesdag = new string[] { "Nw", "Eng", "IT", "IT", "Frans", "Beec", "Wiskune" };
+
+
+        }
+    }
+}
