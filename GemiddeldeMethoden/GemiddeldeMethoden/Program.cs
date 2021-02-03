11178b6eff93e47fbd5c4567ccce8a1890d2f6f6
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-02-03 20:25:41 +0100

    GemiddeldeMethoden

---
+++ GemiddeldeMethoden/GemiddeldeMethoden/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * ANALYSE:
+ * =======
+ * VRAAG getal1
+ * VRAAG getal2
+ * BEREKEN gemiddelde (BerekenGemiddelde: getal1, getal2)
+ * TOON Gemiddelde
+ * 
+ * 
+ * RETURN Gemiddelde
+ 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemiddeldeMethoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //d
+            double getal1, getal2, gemiddelde;
+
+            //i
+            Console.Write("Geef een getal in: ");
+            sbyte = 
+
+        }
+    }
+}
---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //d
+            string tekst, tekstInHoofdletters;
+
+            //i
+            Console.Write("Welke tekst moet in hoofdletters? ");
+            tekst = Console.ReadLine()
+            
+            //
+        }
+
+        private static void TekstNaarHoofdlettersOmzetten()
+        {
+
+        }
+    }
+}
