f7eaa612517c78dfe3bcd248586645b6f2d5f97b
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-02-15 09:08:19 +0100

    HelloWold

---
+++ Goeiedag/Goeiedag/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goeiedag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Goeiedag Yoran");
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Goeiedag2.0/Goeiedag2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goeiedag2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam;
+
+            Console.WriteLine("Wat is uw naam? ");
+            Console.ReadLine();
+            Console.WriteLine("Welkom" { naam });
+        }
+    }
+}
