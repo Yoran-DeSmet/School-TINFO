276b116a7b41458b9652d040d417afdd330c25c8
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-02-17 12:37:30 +0100

    Probeersel

---
+++ Probeersel/Probeersel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probeersel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime geboortedatum;
+
+            
+        }
+    }
+}
