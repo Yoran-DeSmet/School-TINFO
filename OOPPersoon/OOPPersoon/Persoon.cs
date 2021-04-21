222162670218799d28cc8f061c58612cd88698c7
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2021-04-21 12:06:09 +0200

    OOPPersoon

---
+++ OOPPersoon/OOPPersoon/Persoon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPersoon
+{
+    class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public short Geboortedatum { get; set; }
+        public short Geboortejaar { get; set; }
+    }
+}
